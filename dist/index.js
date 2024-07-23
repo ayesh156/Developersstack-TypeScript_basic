1904077fa1b15c366e1cab4e0d2556bd3b07f72a
= <=>
2024-07-23 16:16:45 +0530

    Third Commit

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,4 @@
 "use strict";
-function dev(num1, num2) {
-    if (num2 == 0) {
-        throw new Error('Wrong Input!');
-    }
-    return num1 / num2;
-}
-try {
-    let answer = dev(10, 0);
-    console.log(answer);
-}
-catch (error) {
-    console.error(error);
-}
-finally {
-    console.log('finally');
-}
+const studentName = "name";
+const studentAge = "age";
+const studentAvailability = "isActive";
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-//=============== Error Handling
+//=============== key of
 
-function dev(num1:number,num2:number){
-    if(num2==0){
-        throw new Error('Wrong Input!');
-    }
-    return num1/num2;
-}
+const studentName = "Eshara Ranaweera";
+type NameData = typeof studentName;
 
-try{
-    let answer = dev(10,0);
-console.log(answer);
-}catch(error){
-    console.error(error);
-}finally{
-    console.log('finally');
-}
+const nimalName:string = studentName.toUpperCase();
